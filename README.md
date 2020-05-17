b841632fbc31362690204657ae71db734e9dc41c
Oleg Adianov <oleg.adianov@gmail.com>
2020-05-17 17:13:21 +0300

    Всё вроде работает, можно баловаться. :)

---
+++ README.md
@@ -0,0 +1,7 @@
+#Магический шар.
+
+Написан по приколу и для изучения программирования на Go. :)
+
+#Magick ball.
+
+It made for fun and to learn programming on Go. ;)
--- game.go
+++ game.go
@@ -1,23 +1,31 @@
 package main
 
 import (
-	"fmt"
+	"errors"
 	"image/color"
 	"log"
+	"math/rand"
+	"path/filepath"
 
 	"github.com/hajimehoshi/ebiten"
 	"github.com/hajimehoshi/ebiten/ebitenutil"
 )
 
 type Game struct {
-	// текстуры
-	textures []*ebiten.Image
+	// шар
+	ball *ebiten.Image
+	// стекло
+	glass *ebiten.Image
+	// текстуры ответов
+	answers []*ebiten.Image
 	// цвет фона
 	bg color.RGBA
-	// альфа канал всплывающей текстуры
+	// параметры текущей текстуры
 	alpha float64
+	angle float64
+	rand  int
 	// состояние автомата:
-	// 0 - начало, ничего не делаем
+	// 0 - начало: генерим случайности
 	// 1 - текстура всплывает
 	// 2 - текстура показывается
 	// 3 - текстура тонет
@@ -27,55 +35,92 @@ type Game struct {
 func (g *Game) Init() {
 	// инициализация
 	// грузим текстуры
+
+	// шар:
 	img, _, err := ebitenutil.NewImageFromFile("pics/ball.png", ebiten.FilterDefault)
 	if err != nil {
 		log.Fatal("Could not load resurse: ", err)
 	}
+	g.ball = img
 
-	// собираем их в слайс
-	g.textures = append(g.textures, img)
+	// стекло:
+	img, _, err = ebitenutil.NewImageFromFile("pics/glass.png", ebiten.FilterDefault)
+	if err != nil {
+		log.Fatal("Could not load resurse: ", err)
+	}
+	g.glass = img
 
-	for i := 0; i < 10; i++ {
-		name := fmt.Sprintf("pics/ans_%02d.png", i)
+	// ответы:
+	fl, err := filepath.Glob("./pics/ans_*.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+	//log.Printf("%#v\n", fl)
+
+	for _, name := range fl {
 		img, _, err = ebitenutil.NewImageFromFile(name, ebiten.FilterDefault)
 		if err != nil {
 			log.Fatal("Could not load resurse: ", err)
 		}
 
 		// собираем их в слайс
-		g.textures = append(g.textures, img)
+		g.answers = append(g.answers, img)
 	}
 
 	// устанавливаем цвет фона
-	g.bg = color.RGBA{R: 128, G: 128, B: 128, A: 255}
+	g.bg = color.RGBA{R: 0x51, G: 0x9c, B: 0x52, A: 0xff}
+	// началный альфа канал
 	g.alpha = 0
+	// начальное состояние конечного автомата
 	g.state = 0
 }
 
 func (g *Game) Update(screen *ebiten.Image) error {
-	log.Printf("State: %v\n", g.state)
+	// проверяем нажатие клавишь
+	switch {
+	case ebiten.IsKeyPressed(ebiten.KeyEscape):
+		// если нажата Esc - выходим из игры возвращая сообщение об ошибке
+		// не знаю на сколько правильно так выходить, но пока не понял как сделать лучше в ebiten
+		return errors.New("\nGame finished\n")
+	}
 
 	switch g.state {
 	case 0:
-		return nil
+		// если клацнули мышкой - меняем состояние автомата
+		if ebiten.IsMouseButtonPressed(ebiten.MouseButtonLeft) {
+			g.state++
+			// случайные небольшой угол поворота
+			g.angle = 0.45/2 - rand.Float64()*0.45
+			// случайный ответ
+			g.rand = rand.Intn(len(g.answers))
+		}
 	case 1:
+		// если всплытие - увеличиваем альфа канал для плавного появления
 		if g.alpha > 1 {
+			// достигли 1-ы в альфа канале - меняем состояние автомата
 			g.alpha = 1
 			g.state++
-			return nil
+		} else {
+			// добавляем приращение в альфа канал для плавного появления
+			g.alpha += 0.015
 		}
-		g.alpha += 0.015
-		log.Printf("Alpha: %v\n", g.alpha)
 	case 2:
-		return nil
+		// если клацнули мышкой - меняем состояние автомата
+		if ebiten.IsMouseButtonPressed(ebiten.MouseButtonLeft) {
+			g.state++
+		}
 	case 3:
+		// если альфа меньше 0.02, значит изображение почти исчезло и можно его убирать
+		// и менять состояние автомата на нулевое
 		if g.alpha < 0.02 {
 			g.alpha = 0
 			g.state = 0
-			return nil
+		} else {
+			// инаяе - вычитаем из альфа канала приращение для плавного затухания
+			g.alpha -= 0.015
 		}
-		g.alpha -= 0.015
 	}
+
 	return nil
 }
 
@@ -88,24 +133,40 @@ func (g *Game) Draw(screen *ebiten.Image) {
 	// отрисовываем шар
 	op := &ebiten.DrawImageOptions{}
 	// берём размеры текстуры
-	w, h := g.textures[0].Size()
+	w, h := g.ball.Size()
 	// смещаем текстуру в её центр
 	op.GeoM.Translate(-float64(w)/2, -float64(h)/2)
 	// берём размер экрана
 	sw, sh := screen.Size()
 	// смещаем текстуру туда
 	op.GeoM.Translate(float64(sw)/2, float64(sh)/2)
-	screen.DrawImage(g.textures[0], op)
+	screen.DrawImage(g.ball, op)
 
 	// если состояние автомата больше 0 - отрисовываем ответ
 	if g.state > 0 {
 		op = &ebiten.DrawImageOptions{}
-		w, h = g.textures[1].Size()
+		w, h = g.answers[g.rand].Size()
+
+		op.Filter = ebiten.FilterLinear
 		op.GeoM.Translate(-float64(w)/2, -float64(h)/2)
+		op.GeoM.Rotate(g.angle)
 		op.GeoM.Translate(float64(sw)/2, float64(sh)/2)
 		op.ColorM.Scale(1, 1, 1, g.alpha)
-		screen.DrawImage(g.textures[1], op)
+		screen.DrawImage(g.answers[g.rand], op)
 	}
+
+	// отрисовываем стекло
+	op = &ebiten.DrawImageOptions{}
+	// берём размеры текстуры
+	w, h = g.glass.Size()
+	// смещаем текстуру в её центр
+	op.GeoM.Translate(-float64(w)/2, -float64(h)/2)
+	// берём размер экрана
+	sw, sh = screen.Size()
+	// смещаем текстуру туда
+	op.GeoM.Translate(float64(sw)/2, float64(sh)/2)
+	screen.DrawImage(g.glass, op)
+
 }
 
 func (g *Game) Layout(outW, outH int) (screenWidth, screenHeight int) {
